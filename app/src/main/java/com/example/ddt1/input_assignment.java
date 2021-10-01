7cbb80e6aea39c0a22f70454fc73d02cfaa8db49
Michael Jamie <kramstyles@outlook.com>
2021-10-01 16:12:05 +0100

    made seekbar display value on textview

--- app/src/main/java/com/example/ddt1/input_assignment.java
+++ app/src/main/java/com/example/ddt1/input_assignment.java
@@ -3,12 +3,38 @@ package com.example.ddt1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.SeekBar;
 
 public class input_assignment extends AppCompatActivity {
+    SeekBar newSeekBar;
+    TextView lbl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_assignment);
+
+        newSeekBar = findViewById(R.id.range);
+        lbl = findViewById(R.id.rangevalue);
+
+        newSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                lbl.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+
 }
