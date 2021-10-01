97092a8ec6e7ec544aa968de744d8f31894c6124
Michael Jamie <kramstyles@outlook.com>
2021-10-01 14:50:11 +0100

    added button to assignment

--- app/src/main/java/com/example/ddt1/choose.java
+++ app/src/main/java/com/example/ddt1/choose.java
@@ -2,7 +2,9 @@ package com.example.ddt1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class choose extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class choose extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+    }
+    public void choosee(View v){
+        Intent intent = new Intent(this,input.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/ddt1/input.java
+++ app/src/main/java/com/example/ddt1/input.java
@@ -1,11 +1,14 @@
 package com.example.ddt1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -16,7 +19,7 @@ public class input extends AppCompatActivity {
     Spinner spinner;
     SeekBar seekBar;
     CheckBox rice, beans;
-    Button btn;
+    Button btn, btnAssignment;
 
 
     @Override
@@ -32,6 +35,11 @@ public class input extends AppCompatActivity {
         rice = findViewById(R.id.rice);
         beans = findViewById(R.id.beans);
         btn = findViewById(R.id.btnSend);
+        btnAssignment = findViewById(R.id.btnAssignment);
+        AlertDialog.Builder alertDiag = new AlertDialog.Builder(this);
+        alertDiag.setTitle("Message");
+        alertDiag.setCancelable(false);
+
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +47,47 @@ public class input extends AppCompatActivity {
                 String firstName =fname.getText().toString();
                 if (firstName.trim().length() > 2 ){
                     Toast.makeText(getApplicationContext(), firstName, Toast.LENGTH_LONG).show();
+                } else {
+                    fname.setError("More than 2 Characters needed");
+
                 }
+            }
+        });
+
+
+        btnAssignment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(input.this, input_assignment.class);
+                startActivity(intent);
+            }
+        });
+
+        rice.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    alertDiag.setMessage("Two needed");
+                    AlertDialog alert = alertDiag.create();
+                    alert.show();
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(getApplicationContext(), String.valueOf(progress), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
